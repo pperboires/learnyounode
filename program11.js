d9dd0aa9dcef208ce454c213a271c52f15ff8aa9
Phelipe Perboires <phelipe.perboires@gmail.com>
2015-06-14 17:16:45 -0300

    step 11

---
+++ program11.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var fs = require('fs');
+
+var port = process.argv[2];
+var fileToResponse = process.argv[3];
+
+if (port == undefined)
+{
+	console.error('port undefined.');
+	return;
+}
+
+if (fileToResponse == undefined)
+{
+	console.error('file to response undefined.');
+	return;
+}
+
+var server = http.createServer(function(request, response)
+{
+	var readStream = fs.createReadStream(fileToResponse);
+
+	readStream.on('open', function () {
+    	readStream.pipe(response);
+  	});
+
+  	readStream.on('error', function(err) {
+    	response.end(err);
+  	});
+
+});
+
+console.log('port: '+port);
+
+server.listen(port);
